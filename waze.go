42b1c1f8978ac01a1f7df93257809adfcd49e9e8
philippirsl <philippi.rsl@gmail.com>
2024-06-03 10:59:05 -0300

    Update waze.go

--- waze.go
+++ waze.go
@@ -75,7 +75,7 @@ var (
 			"bottom": -27.5000,
 		},
 		requestURL:       "https://www.waze.com/row-rtserver/web/TGeoRSS?tk=community&format=JSON",
-		broadcastFeedURL: "https://www.waze.com/row-rtserver/broadcast/BroadcastRSS?buid=22c8ece8ae5b984902e7d1c69f5db4bf&format=JSON",
+		broadcastFeedURL: "https://www.waze.com/row-rtserver/broadcast/BroadcastRSS?buid=xxxxxxxxxxxxx&format=JSON",
 	}
 
 	alerts       []map[string]interface{}
